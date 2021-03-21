3cfe2404c95b6fe818361928fdcc4e7a797e33be
gceylan <gulsumceylan1998@gmail.com>
2021-03-21 14:59:34 +0300

    20. gün commit2

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup,FormBuilder, FormControl, Validators} from '@angular/forms'
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-add',
@@ -10,7 +12,9 @@ export class ProductAddComponent implements OnInit {
 
   productAddForm : FormGroup;
 
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder:FormBuilder, 
+              private productService: ProductService,
+              private toastrService: ToastrService) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
@@ -23,5 +27,22 @@ export class ProductAddComponent implements OnInit {
       unitsInStock: ["",Validators.required],
       categoryId : ["",Validators.required]
     })
+  }
+
+  add(){
+    if(this.productAddForm.valid){
+      let productModule = Object.assign({},this.productAddForm.value) 
+      this.productService.add(productModule).subscribe(response=>{
+        console.log(response)
+        this.toastrService.success(response.message,"Başarılı")
+      },responseError=>{
+        console.log(responseError)
+        this.toastrService.error(responseError.error)
+      })
+      
+    }else{
+      this.toastrService.error("Formunuz eksik","Dikkat")
+    }
+
   }
 }
